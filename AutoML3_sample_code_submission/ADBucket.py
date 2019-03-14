57f55ad968728668e45cc94428a5d2244c117599
bharanisruthi <bharanisruthi17@gmail.com>
2019-03-14 12:53:15 -0500

    Update ADBucket.py

--- AutoML3_sample_code_submission/ADBucket.py
+++ AutoML3_sample_code_submission/ADBucket.py
@@ -1,7 +1,13 @@
 class ADBucket(object):
-    """docstring for ADBucket."""
+   """create a new Bucket."""
 
     def __init__(self, capacity=0, content=0.0):
+        
+        """
+        capacity: number of buckets which are aggregating
+        content: Sum of elements in the bucket
+        """
+        
         super(ADBucket, self).__init__()
         self.capacity = capacity
         self.content = content
